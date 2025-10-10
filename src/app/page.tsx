3701d0d2618c85b8db4e3eab454588bac29b50aa
Mihreteab <mihreteabdemeke@gmail.com>
2025-10-10 16:05:32 +0300

    chore: add prettier to format and husky to check code standard before commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-
 export default function Home() {
   return (
     <div className="bg-background text-foreground">
-      <h1>Hello, I’m Mehedi Hasan, 
-      Front-end Developer </h1>
+      <h1>Hello, I’m Mehedi Hasan, Front-end Developer </h1>
     </div>
   );
 }
